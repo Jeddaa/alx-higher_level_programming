78e0c3aa4cce6fc766d5374e7625d7913a3bee39
Jeddaa <fathiat0@gmail.com>
2022-07-04 14:13:50 +0100

    task 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for a in range(0, 8):
-    for b in range (a+1, 10):
+    for b in range(a+1, 10):
         if a != b and a < b:
             print("{}{}".format(a, b), end=', ')
 print("{}{}".format(a+1, b))
