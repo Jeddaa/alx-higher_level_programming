fe67e5a651971ba3030debb034e770e7d8dd3cc1
Jeddaa <fathiat0@gmail.com>
2022-07-07 15:26:16 +0100

    task 3

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-if set_1 is not None and set_2 is not None:
-    def common_elements(set_1, set_2):
+def common_elements(set_1, set_2):
+    if set_1 is not None and set_2 is not None:
         for i in set_1 and set_2:
             return i
