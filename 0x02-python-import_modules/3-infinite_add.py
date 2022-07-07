f23b831e164fe338e15ce1a815bc12e307617c9d
Jeddaa <fathiat0@gmail.com>
2022-07-07 10:18:02 +0100

    task 3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,6 +2,6 @@
 if __name__ == "__main__":
     import sys
     new = 0
-    for i in range(1, len(argv)):
-        new += argv[i]
+    for i in range(1, len(sys.argv)):
+        new += sys.argv[i]
     print('{:d}'.format(new))
