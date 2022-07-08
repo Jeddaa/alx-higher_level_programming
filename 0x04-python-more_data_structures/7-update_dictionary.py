80a9614fdf669ba8a0c6864df820fc3a5f2f8dc9
Jeddaa <fathiat0@gmail.com>
2022-07-08 10:35:16 +0100

    task 7

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def update_dictionary(a_dictionary, key, value):
+     if a_dictionary is not None:
+         a_dictionary.update({key: value})
+         for key, value in sorted(a_dictionary.items()):
+            print("{}: {}".format(key, value))
