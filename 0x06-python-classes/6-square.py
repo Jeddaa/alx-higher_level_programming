ad3c8099e7f61029d55efdfee959c3e92c98285a
Jeddaa <fathiat0@gmail.com>
2022-07-26 04:31:42 +0100

    task 6

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -5,8 +5,8 @@
 class Square:
     """create a square instance"""
     def __init__(self, size=0, position=(0, 0)):
-        self.__size = size
-        self.__position = position
+        self.size = size
+        self.position = position
 
     """retrieve size"""
     @property
