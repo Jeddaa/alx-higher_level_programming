b3b691bbecd57ed007003f3c4df2d9da4fded13d
Jeddaa <fathiat0@gmail.com>
2022-07-26 04:33:14 +0100

    task 6

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -47,8 +47,12 @@ class Square:
         if self.__size == 0:
             print("")
             return
-        [print("") for i in range(0, self.__position[1])]
-        for i in range(0, self.__size):
-            [print(" ", end="") for j in range(0, self.__position[0])]
-            [print("#", end="") for k in range(0, self.__size)]
-            print("")
+        else:
+            for line in range(self.__position[1]):
+                print("")
+            for i in range(self.__size):
+                for space in range(self.__position[0]):
+                    print(" ", end="")
+                for j in range(self.__size):
+                    print('#', end="")
+                print("")
