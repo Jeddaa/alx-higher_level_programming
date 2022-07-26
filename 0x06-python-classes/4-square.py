b405cf6a5467fc485780ec118345ff4401708e49
Jeddaa <fathiat0@gmail.com>
2022-07-26 02:58:43 +0100

    task 4

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Define a class called square"""
 
+
 class Square:
     """The square class"""
     def __init__(self, size=0):
@@ -15,7 +16,7 @@ class Square:
         if type(value) is not int:
             raise TypeError("size must be an integer")
         elif value < 0:
-            raise ValueError ("size must be >= 0")
+            raise ValueError("size must be >= 0")
         else:
             self.__size = value
 
