6f3b614ee07e7f348bc0201f6a67c714e42c2ed3
Jeddaa <fathiat0@gmail.com>
2022-07-29 16:21:57 +0100

    task 2

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -6,8 +6,8 @@ def matrix_divided(matrix, div):
     """function to divide all elements of a matrix"""
     new_matrix, new_row = [], []
     prev_len = 0
-    msg = ('matrix must be a matrix (list of lists) of integers/floats',\
-            'each row of the matrix must have the same size',\
+    msg = ('matrix must be a matrix (list of lists) of integers/floats',
+        'each row of the matrix must have the same size',
             'div must be a number', 'division by zero')
     if type(matrix) is not list:
         raise TypeError(msg[0])
