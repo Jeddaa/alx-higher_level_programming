d75b8d6ada5fd78921ea3600fd2d8555f5352b99
jeddaa <fathiato@gmail.com>
2022-08-01 23:41:57 +0100

    task 11

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Define a class"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """class square"""
+    def __init__(self, size):
+        """initializing square"""
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+    def __str__(self):
+        """defining string"""
+        sq = "[Square] " + str(self.__size) + "/" + str(self.__size)
+        return sq
