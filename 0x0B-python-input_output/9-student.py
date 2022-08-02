665dad829b311cb432c41697649a382a39501c1c
Jeddaa <fathiato@gmail.com>
2022-08-02 13:23:04 -0700

    task 9

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -8,5 +8,7 @@ class Student():
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
+
     def to_json(self):
+        """returns dictionary representation of student"""
         return(vars(self))
