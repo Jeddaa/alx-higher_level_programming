bec367477731732cf7f7d5c9ad660b76e9af135c
Jeddaa <fathiato@gmail.com>
2022-08-02 08:31:44 -0700

    task 1

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -6,4 +6,4 @@
 def write_file(filename="", text=""):
     """writes a string to a file"""
     with open(filename, "w", encoding="utf-8") as file1:
-        file1.write(text)
+       return(file1.write(text))
