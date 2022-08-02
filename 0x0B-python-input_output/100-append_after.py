bc744d8ed8bba3577b3e1a5a962d094712d41681
jeddaa <fathiato@gmail.com>
2022-08-03 00:37:24 +0100

    task 13

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -5,6 +5,5 @@ after each line containing a specific string"""
 
 def append_after(filename="", search_string="", new_string=""):
     with open(filename, "a+", encoding="utf-8") as file1:
-        file2 = list(file1)
-        file2.index(search_string)
+        file2.find(search_string)
         print(file2.write(new_string))
