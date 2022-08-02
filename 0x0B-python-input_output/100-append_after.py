646484ed2de1477703cf3886842340375c8ca1a3
jeddaa <fathiato@gmail.com>
2022-08-03 00:08:06 +0100

    task 13

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""a function that inserts a line of text to a file,
+after each line containing a specific string"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    with open(filename, "a", encoding="utf-8") as file1:
+        file2 = file1.find(search_string)
+        return(file2.write(new_string))
