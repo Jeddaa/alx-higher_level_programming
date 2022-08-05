d28096f9ce783b63fc09d2569350deaf34a63105
jeddaa <fathiato@gmail.com>
2022-08-05 23:14:21 +0100

    task 2

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+"""Define a class Rectangle that inherits from Base"""
+from base import Base
+
+
+class Rectangle(Base):
+    """ the class Rectangle that inherits from class Base"""
+    def __init__(self, width, height, x=0, y=0, id=None):
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
+        """Call to super function to have
+        access to all attributes/methods"""
+        super().__init__(id)
+    
+    """retrieving the width"""
+    @property
+    def width(self):
+        return self.__width
+
+    """setting the width"""
+    @width.setter
+    def width(self, value):
+        self.__width = value
+    
+    """retrieving the height"""
+    @property
+    def height(self):
+        return self.__height
+
+    """setting the height"""
+    @height.setter
+    def height(self, value):
+        self.__height = value
+    
+    """retrieving x"""
+    @property
+    def x(self):
+        return self.__x
+
+    """setting x"""
+    @x.setter
+    def x(self, value):
+        self.__x = value
+
+    """retrieving y"""
+    @property
+    def y(self):
+        return self.__y
+
+    """setting the width"""
+    @width.setter
+    def y(self, value):
+        self.__y = value
