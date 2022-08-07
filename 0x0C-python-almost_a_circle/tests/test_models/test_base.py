8117f6eeae7f38a4140c464b3c68925beadb6e07
jeddaa <fathiato@gmail.com>
2022-08-07 03:02:59 +0100

    task 0

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -2,26 +2,30 @@
 """Unittests for base."""
 
 
+import json
 import unittest
-Base = __import__ ('models.base').Base
+from models.base import Base
+from models.rectangle import Rectangle
+from models.square import Square
 
 
 class TestBase(unittest.TestCase):
     """Define unittests for base"""
 
-    def test_base_without_id_value(self):
+    def test_base_with_no_args(self):
         """Test without a value for id"""
         b1 = Base()
         self.assertEqual(b1.id(), 1)
     
-    def test_base_with_id_value(self):
+    def test_base_with_one_arg(self):
         """Test with a value for id"""
-        b1 = Base()
-        self.assertEqual(b1.id(12), 12)
+        b1 = Base(12)
+        self.assertEqual(b1.id(), 12)
     
-    def test_base_without_id_value(self):
+    def test_multiple_bases(self):
         """Test without a value for id multiple times"""
         b1 = Base()
         b1.id()
-        b1.id()
-        self.assertEqual(b1.id(), 2)
+        b2.id()
+        self.assertEqual(b1.id(), b2.id(), 2)
+
