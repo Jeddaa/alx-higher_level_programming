5e6a5ebd509998615d3406d5a075ec34f1dc9b94
jeddaa <fathiato@gmail.com>
2022-08-07 05:58:33 +0100

    task 0

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+import unittest
+from models.base import Base
+
+
+class TestRectangle(unittest.TestCase):
+    """Define unittests for rectangle"""
+    def test_rectangle_isinstance_base():
+        """Test if Rectangle is an instance of Base"""
+        self.assertIsInstance(Rectangle(13, 9), Base)
+
+    def test_rectangle_with_no_id_args():
+        """testing with no value"""
+        self.assertEqual(Rectangle(10, 2).id, 1)
+
+    def test_rectangle_with_no_args(self):
+        with self.assertRaises(TypeError):
+            print(Rectangle())
+
+    def test_rectangle_with_one_args(self):
+        with self.assertRaises(TypeError):
+            print(Rectangle(10))
+
+    def test_multiple_rectangle(self):
+        """Test without a value for id multiple times"""
+        rec1 = Rectangle(8, 18)
+        rec2 = Rectangle(12, 9)
+        self.assertEqual(rec1.id, rec2.id - 1)
+
+    def test_multiple_rectangle(self):
+        """Test without a value for id multiple times"""
+        rec1 = Rectangle(8, 18)
+        rec2 = Rectangle(12, 9)
+        self.assertEqual(rec1.id, 2)
+        self.assertEqual(rec2.id, 3)
