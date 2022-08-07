860704887afd6bb2763db2ab36f59930a77cd78c
jeddaa <fathiato@gmail.com>
2022-08-07 06:06:53 +0100

    task 0

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -7,21 +7,13 @@ from models.rectangle import Rectangle
 
 class TestRectangle(unittest.TestCase):
     """Define unittests for rectangle"""
-    def test_rectangle_isinstance_base():
+    def test_rectangle_isinstance_base(self):
         """Test if Rectangle is an instance of Base"""
         self.assertIsInstance(Rectangle(13, 9), Base)
 
-    def test_rectangle_with_no_id_args():
+    def test_rectangle_with_no_id_args(self):
         """testing with no value"""
         self.assertEqual(Rectangle(10, 2).id, 1)
-
-    def test_rectangle_with_no_args(self):
-        with self.assertRaises(TypeError):
-            print(Rectangle())
-
-    def test_rectangle_with_one_args(self):
-        with self.assertRaises(TypeError):
-            print(Rectangle(10))
 
     def test_multiple_rectangle(self):
         """Test without a value for id multiple times"""
@@ -35,3 +27,17 @@ class TestRectangle(unittest.TestCase):
         rec2 = Rectangle(12, 9)
         self.assertEqual(rec1.id, 2)
         self.assertEqual(rec2.id, 3)
+
+    def test_rectangle_with_no_args(self):
+        """testing with no argument in rectangle()"""
+        with self.assertRaises(TypeError):
+            print(Rectangle())
+
+    def test_rectangle_with_one_args(self):
+        """testing with only one argument in rectangle()"""
+        with self.assertRaises(TypeError):
+            print(Rectangle(10))
+
+    
+
+    
