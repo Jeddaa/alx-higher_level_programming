7f4e33f0e047428460f835dafe0f74487f7bc422
jeddaa <fathiato@gmail.com>
2022-08-07 06:42:55 +0100

    task 0

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -22,37 +22,116 @@ class TestRectangle(unittest.TestCase):
             print(Rectangle(10))
 
     def test_rectangle_with_two_args(self):
-        """Test without a value for id multiple times"""
+        """Test with two arguments"""
         rec1 = Rectangle(8, 18)
         rec2 = Rectangle(12, 9)
         self.assertEqual(rec1.id, rec2.id - 1)
 
     def test_multiple_rectangle(self):
-        """Test without a value for id multiple times"""
+        """Test with two arguments"""
         rec1 = Rectangle(8, 18)
         rec2 = Rectangle(12, 9)
         self.assertEqual(rec1.id, 1)
         self.assertEqual(rec2.id, 2)
 
     def test_rectangle_with_three_args(self):
-        """Test without a value for id multiple times"""
+        """Test with three arguments"""
         rec1 = Rectangle(8, 18, 2)
         rec2 = Rectangle(12, 9, 3)
         self.assertEqual(rec1.id, rec2.id - 1)
 
     def test_rectangle_with_four_args(self):
-        """Test without a value for id multiple times"""
+        """Test with four arguments"""
         rec1 = Rectangle(8, 18, 2, 1)
         rec2 = Rectangle(12, 9, 3, 2)
         self.assertEqual(rec1.id, rec2.id - 1)
 
-    def test_rectangle_with_five_args(self):
-        """Test without a value for id multiple times"""
+    def test_with_five_args(self):
+        """Test with five arguments"""
         rec1 = Rectangle(8, 18, 2, 0, 2)
-        rec2 = Rectangle(12, 9, 3, 2, 1)
-        self.assertEqual(rec1.id, rec2.id - 1)
+        self.assertEqual(rec1.id, 2)
 
-    def test_rectangle_with_six_args(self):
-        """Test without a value for id multiple times"""
+    def test_with_morethan_five_args(self):
+        """Test with more than five arguments"""
         with self.assertRaises(TypeError):
             print(Rectangle(8, 18, 2, 0, 2, 6).id)
+
+    def test_private_width(self):
+        """Test width as a private instance attribute"""
+        with self.assertRaises(AttributeError):
+            print(Rectangle(8, 18, 2, 0, 2).__width)
+
+    def test_private_height(self):
+        """Test height as a private instance attribute"""
+        with self.assertRaises(AttributeError):
+            print(Rectangle(8, 18, 2, 0, 2).__height)
+    
+    def test_private_x(self):
+        """Test x as a private instance attribute"""
+        with self.assertRaises(AttributeError):
+            print(Rectangle(8, 18, 2, 0, 2).__x)
+    
+    def test_private_y(self):
+        """Test y as a private instance attribute"""
+        with self.assertRaises(AttributeError):
+            print(Rectangle(8, 18, 2, 0, 2).__y)
+    
+    def test_private_id(self):
+        """Test id as a private instance attribute"""
+        with self.assertRaises(AttributeError):
+            print(Rectangle(8, 18, 2, 0, 2).__id)
+    
+    def test_width_getter(self):
+        """Test gettin width"""
+        rec1 = Rectangle(8, 18, 2, 0, 2)
+        self.assertEqual(rec1.width, 8)
+
+    def test_width_setter(self):
+        """Test setting width"""
+        rec1 = Rectangle(8, 18, 2, 0, 2)
+        rec1.width = 10
+        self.assertEqual(rec1.width, 10)
+
+    def test_height_getter(self):
+        """Test getting height"""
+        rec1 = Rectangle(8, 18, 2, 0, 2)
+        self.assertEqual(rec1.height, 18)
+
+    def test_height_setter(self):
+        """Test settin height"""
+        rec1 = Rectangle(8, 18, 2, 0, 2)
+        rec1.height = 12
+        self.assertEqual(rec1.height, 12)
+
+    def test_x_getter(self):
+        """Test getting x"""
+        rec1 = Rectangle(8, 18, 2, 0, 2)
+        self.assertEqual(rec1.x, 2)
+    
+    def test_x_setter(self):
+        """Test setting x"""
+        rec1 = Rectangle(8, 18, 2, 0, 2)
+        rec1.x = 3
+        self.assertEqual(rec1.x, 3)
+
+    def test_y_getter(self):
+        """Test gettin y"""
+        rec1 = Rectangle(8, 18, 2, 0, 2)
+        self.assertEqual(rec1.y, 0)
+
+    def test_y_setter(self):
+        """Test setting y"""
+        rec1 = Rectangle(8, 18, 2, 0, 2)
+        rec1.y = 1
+        self.assertEqual(rec1.width, 1)
+
+    def test_id_getter(self):
+        """Test gettin id"""
+        rec1 = Rectangle(8, 18, 2, 0, 2)
+        self.assertEqual(rec1.y, 2)
+
+    def test_id_setter(self):
+        """Test setting id"""
+        rec1 = Rectangle(8, 18, 2, 0, 2)
+        rec1.id = 3
+        self.assertEqual(rec1.id, 3)
