7b2d1a441b2a4888e7c8d0fe64eefc8630808995
jeddaa <fathiato@gmail.com>
2022-08-08 21:42:29 +0100

    task 10

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -6,11 +6,10 @@ from models.rectangle import Rectangle
 class Square(Rectangle):
     """the class Square that inherits from class Rectangle"""
     def __init__(self, size, x=0, y=0, id=None):
-        """Call to super function to have
-        access to all attributes/methods"""
+        """initializing the square class"""
         super().__init__(size, size, x, y, id=None)
         """validation of attribute: size"""
-        self.size = size
+        self.size = self.width
 
 
     @property
