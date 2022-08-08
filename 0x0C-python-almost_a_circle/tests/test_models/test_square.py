0f350e77487c853330918c2ec26a9e79e73981af
jeddaa <fathiato@gmail.com>
2022-08-08 22:47:59 +0100

    task 0

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+import unittest
+from models.base import Base
+from models.rectangle import Rectangle
+from models.square import Square
+
+
+class TestSquare_(unittest.TestCase):
+    """Define unittests for square"""
+    def test_square_with_no_args(self):
+        """testing with no argument in square()"""
+        with self.assertRaises(TypeError):
+            print(Square())
+
+    def test_square_with_one_arg(self):
+        """testing with one argument"""
+        sq = Square(13)
+        self.assertEqual(str(sq), "[Square] (1) 0/0- 10)")
+
