326a17a42f3950c4d68fe0e6b92541e6d3ec33c1
jeddaa <fathiato@gmail.com>
2022-08-08 21:12:59 +0100

    task 1

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -6,24 +6,24 @@ import json
 class Base:
     """the Base class"""
     __nb_objects = 0
-    """the class constructor"""
     def __init__(self, id=None):
+        """the class constructor"""
         if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
-    """returns the JSON representation of list_dictionaries"""
     @staticmethod
     def to_json_string(list_dictionaries):
+        """returns the JSON representation of list_dictionaries"""
         if list_dictionaries is None:
             return []
         return json.dumps(list_dictionaries)
 
-    """returns the list of the JSON string representation"""
     @staticmethod
     def from_json_string(json_string):
+        """returns the list of the JSON string representation"""
         if json_string is None:
             return []
         return json.loads(json_string)
