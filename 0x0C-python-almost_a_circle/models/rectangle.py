0ed2aec3025d965ac848c81e3ad94d0f13598f09
jeddaa <fathiato@gmail.com>
2022-08-08 21:23:41 +0100

    task 7

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -85,8 +85,8 @@ class Rectangle(Base):
 
     def __str__(self):
         """string representation of the rectangle"""
-        return f"[{self.__class__.__name__}] ({self.id}) \
-            {self.__x}/{self.__y} - {self.__width}/{self.__height}"
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id,
+        self.__x, self.__y, self.__width, self.__height)
 
     def update(self, *args, **kwargs):
         """assigning arguments to each attributes using args and kwargs"""
