f89461968ce6e3d5bbc0759d6bf12a5b678f02dd
jeddaa <fathiato@gmail.com>
2022-08-08 21:57:13 +0100

    task 15

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -19,7 +19,7 @@ class Base:
     def to_json_string(list_dictionaries):
         """returns the JSON representation of list_dictionaries"""
         if list_dictionaries is None:
-            return []
+            return "[]"
         return json.dumps(list_dictionaries)
 
     @staticmethod
