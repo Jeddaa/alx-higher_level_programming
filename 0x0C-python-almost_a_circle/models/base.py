fafca959cd9ff574e9852e8181c2c497a432ddb3
Jeddaa <fathiato@gmail.com>
2022-08-08 12:21:27 -0700

    task 1

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -13,21 +13,21 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
-            
+
     """returns the JSON representation of list_dictionaries"""
     @staticmethod
     def to_json_string(list_dictionaries):
         if list_dictionaries is None:
             return []
         return json.dumps(list_dictionaries)
-    
+
     """returns the list of the JSON string representation"""
     @staticmethod
     def from_json_string(json_string):
         if json_string is None:
             return []
         return json.loads(json_string)
-    
+
     @classmethod
     def save_to_file(cls, list_objs):
         """writes the JSON string representation of list_objs to a file"""
