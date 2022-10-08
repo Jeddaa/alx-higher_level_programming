171d0b1d1753580521c8424b8baaed59f5207675
Jeddaa <fathiato@gmail.com>
2022-10-08 19:36:12 +0100

    object relational mapping

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -10,9 +10,10 @@ connet = MySQLdb.connect(host="localhost",
                          user=argv[1],
                          password=argv[2],
                          database=argv[3])
-""" create cursor to execute queries in SQL"""
+#create cursor to execute queries in SQL
+
 cursor = connet.cursor()
-query = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC"
+query = "SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY states.id ASC"
 cursor.execute(query)
 records = cursor.fetchall()
 for i in records:
