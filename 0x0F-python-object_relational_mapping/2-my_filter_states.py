b6611ab9751f9698c24ce7a48361240a49a588ba
Jeddaa <fathiato@gmail.com>
2022-10-08 23:52:54 +0100

    object relational mapping

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -5,17 +5,18 @@ the states table of hbtn_0e_0_usa where name matches the argument."""
 import MySQLdb
 from sys import argv
 
-connet = MySQLdb.connect(host="localhost",
-                         port=3306,
-                         user=argv[1],
-                         password=argv[2],
-                         database=argv[3])
-cursor = connet.cursor()
-query = "SELECT * FROM states WHERE name LIKE '{:s}' \
-            ORDER BY id ASC".format(argv[4])
-cursor.execute(query)
-records = cursor.fetchall()
-for i in records:
-    print(i)
-cursor.close()
-connet.close()
+if __name__ == "__main__":
+    connet = MySQLdb.connect(host="localhost",
+                             port=3306,
+                             user=argv[1],
+                             password=argv[2],
+                             database=argv[3])
+    cursor = connet.cursor()
+    query = "SELECT * FROM states WHERE name LIKE '{:s}' \
+                 ORDER BY id ASC".format(argv[4])
+    cursor.execute(query)
+    records = cursor.fetchall()
+    for i in records:
+        print(i)
+    cursor.close()
+    connet.close()
